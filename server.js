a40121665433145b6e4eb7732b76711b3cb693be
C A <chadandari@outlook.com>
2024-01-24 22:46:06 -0500

    Add employee much cleaner

--- server.js
+++ server.js
@@ -141,6 +141,7 @@ async function addEmployee(){
         for (let i = 0; i<data.length; i++){
             employeeArray.push(data[i].first_name + " " + data[i].last_name);
         }
+        employeeArray.push("No Manager");
     });
 
     let roleArray = [];
@@ -176,26 +177,24 @@ async function addEmployee(){
         },
     ]);
 
-    // console.log(userResponse.employee_firstName);
-    // console.log(userResponse.employee_lastName);
-    // console.log(userResponse.employee_role);
-    // console.log(userResponse.employee_manager);
-
-    //Here we get the ID associated with the employee's manager
-    let manId = await asyncCallId(`SELECT id FROM employees_db.employees WHERE CONCAT (first_name, " ", last_name) LIKE "${userResponse.employee_manager}"`);
-    // console.log("man ID = " + manId);
-
-    //Here we get the ID associated with the employee's role
-    let roleId = await asyncCallId(`SELECT id FROM employees_db.roles WHERE title =  "${userResponse.employee_role}"`);
-    // console.log("role ID = " + roleId);
-
-
-    //Here we put it together to insert the employee into the db.
-    await asyncCallAdd(`INSERT INTO employees_db.employees(first_name, last_name, role_id, manager_id)
-    VALUES  ("${userResponse.employee_firstName}", "${userResponse.employee_lastName}", 
-    ${roleId}, ${manId})`);
-    console.log("Employee Added");
-    viewAllEmployees();
+    if (userResponse.employee_manager === "No Manager"){
+        const str = `INSERT INTO employees_db.employees (first_name, last_name, role_id)
+        VALUES ("${userResponse.employee_firstName}", "${userResponse.employee_lastName}", 
+        (SELECT id FROM employees_db.roles WHERE title LIKE "${userResponse.employee_role}"))`;
+        db.query(str, (err, data) => {
+            console.log("New employee added");
+            viewAllEmployees();
+        });
+    } else{
+        const str = `INSERT INTO employees_db.employees (first_name, last_name, role_id, manager_id)
+        VALUES ("${userResponse.employee_firstName}", "${userResponse.employee_lastName}", 
+        (SELECT id FROM employees_db.roles WHERE title LIKE "${userResponse.employee_role}"), 
+        (SELECT id FROM (SELECT * FROM employees_db.employees WHERE CONCAT (first_name, " ", last_name) LIKE "${userResponse.employee_manager}") AS toPost));`;
+        db.query(str, (err, data) => {
+            console.log("New employee added");
+            viewAllEmployees();
+        });
+    }
 }
 
 
@@ -297,61 +296,6 @@ async function updateEmployeeRole(){
     viewAllEmployees();
 }
 
-
- function buildNewRole(answer){
-    
-
-    let rollId;
-    const sql = `SELECT department_name, id FROM employees_db.departments`;
-    db.query(sql, (err, data) => {
-        console.log(answer);
-        //Check if the deparment already exists. If so, assign the matching existing deparment to the new role.
-        for (let i = 0; i < data.length; i++){
-            if (data[i].department_name.toLowerCase() === answer.role_department.toLowerCase()){
-                console.log(data[i]);
-                console.log("There is an existing department");
-                rollId = data[i].id;
-
-                //If the new role has an existing department, continue with adding the role to the db while assigning
-                //the relevant department_id to the new role entry
-                const roleSql = 
-                `INSERT INTO employees_db.roles(title, salary, department_id)
-                VALUES  ("${answer.role_name}", ${answer.role_salary}, ${rollId})`;
-
-                db.query(roleSql, (err, data) => {
-                    console.log("New role added with old dept");
-                    viewAllRoles();
-                });
-                return;
-            };
-        };
-    });
-
-    //If there's no match, then the new department that the user added will be entered into the db along with the new role.
-    //Add a new department
-    const deptSql = `INSERT INTO employees_db.departments(department_name)
-    VALUES  ("${answer.role_department}")`;
-    db.query(deptSql, (err, data) => {
-        console.log("New department added");
-    });
-
-    //There's a new department, now we get the id by repeating the first for loop
-    const getDeptId = `SELECT id FROM employees_db.departments WHERE department_name =  "${answer.role_department}"`;
-    db.query(getDeptId, (err, data) => {
-        rollId = data[0].id;
-        const roleSql = `INSERT INTO employees_db.roles(title, salary, department_id) 
-        VALUES  ("${answer.role_name}", ${answer.role_salary}, ${rollId})`;
-
-        db.query(roleSql, (err, data) => {
-            console.log(`New Role is ${answer.role_name}`);
-            console.log(`New Salary is ${answer.role_salary}`);
-            console.log(`New Department ID is ${rollId}`);
-            console.log("New role added with new dept");
-
-        viewAllRoles();
-        });
-    });
-}
 
 async function updateInput(roles, employees){
     return inquirer.prompt([
